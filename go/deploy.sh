e27072d5e736f651f365aa096bd43519b6a46875
vastwi <arvindsv@thoughtworks.com>
2015-10-31 11:26:24 +0530

    Correcting twitter address [Arvind]

--- go/deploy.sh
+++ go/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Reconstituting SSH key..."
-echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_geeknight
-chmod 600 id_geeknight
+echo $DEPLOY_KEY | sed 's/\\n/\'$'\n/g' > id_vodqa
+chmod 600 id_vodqa
 
 export GIT_SSH=$(pwd)/go/ssh.sh
-export SSH_KEY=$(pwd)/id_geeknight
+export SSH_KEY=$(pwd)/id_vodqa
 
 set -xe
 
@@ -13,7 +13,7 @@ echo "Cloning gh-pages..."
 rm -Rf gh-pages
 git config --global user.email "snap@snap-ci.org"
 git config --global user.name "snap-ci"
-git clone --quiet --branch=gh-pages git@github.com:TWChennai/geeknight.git gh-pages
+git clone --quiet --branch=gh-pages git@github.com:TWChennai/VODQA-Chennai.git gh-pages
 
 echo "Building site..."
 bundle exec nanoc
@@ -31,4 +31,4 @@ git commit --amend --allow-empty -q -m "Built $SNAP_COMMIT_SHORT"
 git push -fq origin gh-pages
 
 echo "Cleaning up..."
-rm -Rf ~/.ssh/id_geeknight*
+rm -Rf ~/.ssh/id_vodqa*
